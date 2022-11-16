7d72b276e5f0f07754e9b380563d977a6d512447
mio991 <mio991@mio991.de>
2022-11-16 11:04:18 +0100

    add angular material

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'todo-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
   title = 'todo';
